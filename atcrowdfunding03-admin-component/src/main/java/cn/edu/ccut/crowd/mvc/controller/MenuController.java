a85aaa686a91e34d025b0728422a21ffdb4d6f28
Nefelibatacc~ <fengshi_work@outlook.com>
2023-04-12 15:36:54 +0800

    菜单维护部分 菜单维护页面显示树形结构（第二版代码，改进循环嵌套产生的执行效率低问题）

--- atcrowdfunding03-admin-component/src/main/java/cn/edu/ccut/crowd/mvc/controller/MenuController.java
+++ atcrowdfunding03-admin-component/src/main/java/cn/edu/ccut/crowd/mvc/controller/MenuController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -32,8 +34,53 @@ public class MenuController {
     * @Date: 2023/4/12
     */
     @ResponseBody
-    @RequestMapping("/menu/get/whole/tree")
-    public ResultEntity<Menu> getWholeTree(){
+    @RequestMapping("/menu/get/whole/tree.json")
+    public ResultEntity<Menu> getWholeTreeNew(){
+
+        // 1、查询所有的menu对象
+        List<Menu> menuList = menuService.getAll();
+
+        // 2、声明一个变量用来存储找到的根节点
+        Menu root = null;
+
+        // 3、创建一个map对象用来存储id和menu的对应关系便于查找父节点
+        Map<Integer, Menu> menuMap = new HashMap<>();
+
+        // 4、遍历menuList填充menuMap
+        for (Menu menu : menuList) {
+
+            Integer id = menu.getId();
+
+            menuMap.put(id, menu);
+        }
+
+        // 5、再次遍历menuList查找根节点、组装父子节点
+        for (Menu menu : menuList) {
+
+            // 6、获取当前menu对象的pid值
+            Integer pid = menu.getPid();
+
+            // 7、如果pid为null，判定为根节点
+            if (pid == null) {
+                root = menu;
+
+                // 8、如果当前节点是根节点，那么肯定没有父节点，不必继续执行
+                continue;
+            }
+
+            // 9、如果pid不为null，说明当前节点有父节点，那么可以根据pid到menuMap中查找对应的Menu对象
+            Menu father = menuMap.get(pid);
+
+            // 10、当当前节点存入父节点的children集合
+            father.getChildren().add(menu);
+        }
+
+        // 11、经过上面的运算，根节点包含了整个树形结构，返回根节点就是返回整棵树
+        return ResultEntity.successWithData(root);
+    }
+
+
+    public ResultEntity<Menu> getWholeTreeOld(){
 
         // 1、查询所有的menu对象
         List<Menu> menuList = menuService.getAll();
